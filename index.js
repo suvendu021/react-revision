de8ca2cc55adb200b86da656ecf7473ca5d21445
suvendu021 <suvendukumarsahoo2002@gmail.com>
2025-06-21 13:12:32 +0530

    day 2 of react revision

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const heading = React.createElement(
-  "h1",
-  {
-    id: "id_react_heading",
-    color: "rgb(55, 212, 20)",
-  },
-  "hello i am a react heading element"
-);
+//core react things for use react in our app by cdn links
+
+/// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "id_react_heading",
+//     color: "rgb(55, 212, 20)",
+//   },
+//   "hello i am a react heading element"
+// );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root_id_from_indexhtml")
+// );
+// root.render(heading);
+//--------------------------------------------------------------
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HeadingComponent = () => {
+  return (
+    <>
+      <h1>hii i'm jsx🥲{300 + 800}</h1>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root_id_from_indexhtml")
 );
-root.render(heading);
+root.render(<HeadingComponent />);
